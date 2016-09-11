0f4c67db5f3d0be79c30223cb9b28e8898b2d7df
Higor W. Nunes <higorn@gmail.com>
2016-09-10 22:11:28 -0300

    implementação objetos descartáveis

---
+++ src/main/java/br/com/sisnunes/myzkx/data/Disposeable.java
@@ -0,0 +1,10 @@
+package br.com.sisnunes.myzkx.data;/*
+ * File:   Removeable.java
+ *
+ * Created on 10/09/16, 21:50
+ */
+
+public interface Disposeable {
+  public boolean isDiscarted();
+  public void setDiscarted(boolean discarted);
+}
